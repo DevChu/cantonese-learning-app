d4a78b2752829ee91d3fe9951113c778eddaada6
James Chu <marchuqq@gmail.com>
2025-11-22 20:40:25 +0800

    refine for context name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 注意：將 'your-repo-name' 替換為您的 GitHub Repository 名稱
-  // 例如您的 repo 叫 "cantonese-app"，這裡就填 "/cantonese-app/"
-  base: '/cantonese-app/', 
+  base: '/cantonese-learning-app/', 
 })
